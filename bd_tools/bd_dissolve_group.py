90163c5e1e34aaf8bbb50886f13f5f1db51101ac
Alexander Kucera <a.kucera@babylondreams.de>
2017-02-20 11:15:01 +0100

    added bd_dissolve_group_command
    first sketches
    
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

---
+++ bd_tools/bd_dissolve_group.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Alexander Kucera
+# babylondreams.de
+
+# Description
+"""
+babylondreams - bd_dissolve_group
+
+Release Notes:
+
+V0.1 Initial Release - 2017-02-20
+
+"""
+
+import modo
+import lx
+import traceback
+
+
+# FUNCTIONS -----------------------------------------------
+# END FUNCTIONS -----------------------------------------------
+
+# MAIN PROGRAM --------------------------------------------
+def main():
+    scene = modo.Scene()
+
+
+# END MAIN PROGRAM -----------------------------------------------
+
+if __name__ == '__main__':
+    # Argument parsing is available through the 
+    # lx.arg and lx.args methods. lx.arg returns 
+    # the raw argument string that was passed into 
+    # the script. lx.args parses the argument string 
+    # and returns an array of arguments for easier 
+    # processing.
+
+    argsAsString = lx.arg()
+    argsAsTuple = lx.args()
+
+    try:
+        main()
+    except:
+        print traceback.format_exc()
---
+++ lxserv/bd_dissolve_group_command.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+
+babylondreams - bd_command_dissolve_group
+
+Release Notes:
+
+V0.1 Initial Release - 2017-02-20
+
+"""
+
+import babylondreams
+import lx
+import modo
+
+from bd_tools import bd_dissolve_group
+
+__author__ = "Alexander Kucera"
+__copyright__ = "Copyright 2017, BabylonDreams - Alexander & Monika Kucera GbR"
+__credits__ = ["Alexander Kucera"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Alexander Kucera"
+__email__ = "a.kucera@babylondreams.de"
+__status__ = "Development"
+
+
+class CommandClass(babylondreams.CommanderClass):
+    _commander_last_used = []
+
+    def commander_arguments(self):
+        return [
+            arg_commander
+        ]
+
+    def commander_execute(self, msg, flags):
+        dish1 = self.commander_arg_value(0)
+        dish2 = self.commander_arg_value(1)
+
+        modo.dialogs.alert("breakfast", ' and '.join([dish1, dish2]))
+
+
+lx.bless(CommandClass, 'bd.command')
