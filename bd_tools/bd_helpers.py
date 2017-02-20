d9c87c54f91eafcfb47adde6baa4116196dede86
Alexander Kucera <a.kucera@babylondreams.de>
2017-02-20 15:55:29 +0100

    Updated with Dissolve Docs
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

--- bd_tools/bd_helpers.py
+++ bd_tools/bd_helpers.py
@@ -9,6 +9,8 @@ babylondreams - bd_helpers
 
 Release Notes:
 
+    A collection of several often used helper functions.
+
 V0.1 Initial Release - 2017-02-20
 
 """
@@ -18,7 +20,6 @@ import lx
 import traceback
 
 
-# FUNCTIONS -----------------------------------------------
 def selected(num=1):
     """
 
@@ -41,29 +42,3 @@ def selected(num=1):
         selected = selected[-num:]
 
     return selected
-
-# END FUNCTIONS -----------------------------------------------
-
-# MAIN PROGRAM --------------------------------------------
-def main():
-
-    print("Please call a function. This is just a helper module.")
-
-
-# END MAIN PROGRAM -----------------------------------------------
-
-if __name__ == '__main__':
-    # Argument parsing is available through the 
-    # lx.arg and lx.args methods. lx.arg returns 
-    # the raw argument string that was passed into 
-    # the script. lx.args parses the argument string 
-    # and returns an array of arguments for easier 
-    # processing.
-
-    argsAsString = lx.arg()
-    argsAsTuple = lx.args()
-
-    try:
-        main()
-    except:
-        print traceback.format_exc()
