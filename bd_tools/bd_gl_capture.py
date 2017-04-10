d3c50758eb1f3cdbe983988adf7a9b82ee4b5873
Alexander Kucera <a.kucera@babylondreams.de>
2017-04-10 19:41:56 +0200

    typo fix
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

--- bd_tools/bd_gl_capture.py
+++ bd_tools/bd_gl_capture.py
@@ -78,7 +78,7 @@ def capture_path():
 def main(gl_recording_size=1.0, gl_recording_type='image', viewport_camera='rendercam', shading_style='advgl',
          filename='preview', filepath="", first_frame=1001, last_frame=1250, raygl='off', replicators=False,
          bg_style='environment', use_scene_range=True, automatic_naming=True, overwrite=True, bbox_toggle='full',
-         capture_file='playblast', capture_path=HOME_DIR)):
+         capture_file='playblast', capture_path=HOME_DIR):
 
     """
     Preps and calls gl.capture.
