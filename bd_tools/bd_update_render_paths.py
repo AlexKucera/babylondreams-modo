d70c9e6457d51c30c0885d5b3afeff1378c3f9b4
Alexander Kucera <a.kucera@babylondreams.de>
2017-04-11 10:09:15 +0200

    path adjustments
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

--- bd_tools/bd_update_render_paths.py
+++ bd_tools/bd_update_render_paths.py
@@ -47,17 +47,16 @@ def main():
         shot = bd_globals.find_shot_version(scene_path)
         lx.out('File {} is at Version {}.'.format(shot['shotname'], shot['version']))
         project = bd_globals.find_project(scene_path)
-        lx.out(project)
 
         project_config = bd_globals.projectconfig(scene_path)
 
-
-        output = '{project_dir}/{img}/{renders}/{shot}/{file}'.format(
+        output = '{project_dir}/{img}/{renders}/{shot}/{version}/{file}'.format(
             project_dir=project['project_dir'],
             img=project_config['images/parent folder'],
             renders=project_config['images/3d'],
             file=os.path.splitext(file)[0],
-            shot=shot['shotname']
+            shot="{}_{}".format(shot['sequence'], shot['shot']),
+            version=shot['shotname']
         )
 
         lx.out("The renders will be located at: {}".format(output))
@@ -66,49 +65,39 @@ def main():
             os.makedirs(output)
 
         for item in scene.iterItemsFast('renderOutput'):
-            if " " in item.name:
-                enabled = item.channel('enable').get()
 
-                if enabled:
-                    modo.dialogs.alert('Warning',
-                                       "Please use no spaces in renderOutput naming (The offending "
-                                       "output is " + item.name + "). It just screws everything up sooner or later.",
-                                       dtype='warning')
-            else:
+            outputtype = item.channel('effect').get()
+            filepath = item.channel('filename').get()
+            enabled = item.channel('enable').get()
 
-                outputtype = item.channel('effect').get()
-                filepath = item.channel('filename').get()
-                enabled = item.channel('enable').get()
+            if filepath is not None and enabled:
 
-                if filepath is not None and enabled:
+                fileformat = enabled = item.channel('format').get()
 
-                    fileformat = enabled = item.channel('format').get()
-
-                    if fileformat is None or "$FLEX":
-                        if outputtype == "shade.normal" or outputtype == "geo.world" or outputtype == "geo.uv" or outputtype == "depth" or outputtype == "motion":
-                            fileformat = "openexr_32"
-                        else:
-                            fileformat = "openexr"
-
-                    if item.name == "rgba":
-                        renderpasspath = output
+                if fileformat is None or "$FLEX":
+                    if outputtype == "shade.normal" or outputtype == "geo.world" or outputtype == "geo.uv" or outputtype == "depth" or outputtype == "motion":
+                        fileformat = "openexr_32"
                     else:
-                        renderpasspath = "{}_{}/".format(output, item.name)
+                        fileformat = "openexr"
 
-                    renderoutputpath = renderpasspath + os.path.splitext(file)[0] + "_"
-                    lx.out("RenderOutput " + item.name + " will be located at: " +
-                           renderpasspath)
-                    if not os.path.exists(renderpasspath):
-                        os.makedirs(renderpasspath)
-                    lx.out("Setting Render Output path to: " + renderoutputpath)
-                    item.channel('filename').set(renderoutputpath)
-                    lx.out("Setting Render Output format to: " + fileformat)
-                    item.channel('format').set(fileformat)
-
+                if item.name == "rgba":
+                    renderpasspath = "{}/".format(output)
                 else:
+                    renderpasspath = "{}/{}/".format(output, item.name.replace(" ", "_"))
 
-                    lx.out('Skipping {} as it is either disabled or has no "PATH" filled in'.format(item.name))
+                renderoutputpath = renderpasspath + os.path.splitext(file)[0] + "_"
+                lx.out("RenderOutput " + item.name + " will be located at: " +
+                       renderpasspath)
+                if not os.path.exists(renderpasspath):
+                    os.makedirs(renderpasspath)
+                lx.out("Setting Render Output path to: " + renderoutputpath)
+                item.channel('filename').set(renderoutputpath)
+                lx.out("Setting Render Output format to: " + fileformat)
+                item.channel('format').set(fileformat)
 
+            else:
+                pass
+                # lx.out('Skipping {} as it is either disabled or has no "PATH" filled in'.format(item.name))
 
     bd_helpers.timer(start_timer, ' Overall')
 
