6ca9fc024645e67b253efc559886cbde7ea818df
Alexander Kucera <a.kucera@babylondreams.de>
2017-07-20 17:34:04 +0200

    first working auto transfer anim version
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

--- bd_tools/bd_transfer_anim_auto.py
+++ bd_tools/bd_transfer_anim_auto.py
@@ -23,85 +23,58 @@ import traceback
 from pprint import pprint
 
 from bd_tools import bd_helpers
+import bd_transfer_anim
 
 
 # FUNCTIONS -----------------------------------------------
 
-def get_channels(item, type=None, forbidden_channels=[], isAnimated=False):
-    item_channels = []
-    for channel in item.channels():
-        if channel.name not in forbidden_channels:
-            if type is "name":
-                item_channels.append(channel.name)
-            if type is "index":
-                item_channels.append((channel.index))
-            if type is None:
-                pass
-    if type is None:
-        item_channels = item.channels()
-    return item_channels
-
-
-def channel_copy_paste(item_id, channel_name, cmd="copy"):
-    lx.eval("select.channel {{{0}:{1}}} set".format(item_id, channel_name))
-    if cmd is "paste":
-        lx.eval("channel.paste")
-    elif cmd is "copy":
-        lx.eval("channel.copy")
-
+def get_tags(item):
+    print item
+    if item.hasTag("anim"):
+        if item.readTag("anim"):
+            tag = item.readTag("anim")
+            return tag
+    return None
 
 # END FUNCTIONS -----------------------------------------------
 
 # MAIN PROGRAM --------------------------------------------
 def main():
-    forbidden_channels = ["localMatrix", "wposMatrix", "wrotMatrix", "wsclMatrix", "wpivPosMatrix", "wpivRotMatrix",
-                          "worldMatrix", "glstate", "crvGroup", "matrix"]
+
     scene = modo.Scene()
     selected = bd_helpers.selected(2)
 
-    source = selected[0]
-    target = selected[1]
+    sourceGroup = selected[0]
+    targetGroup = selected[1]
 
-    source_channels = get_channels(source, type="name", forbidden_channels=forbidden_channels, isAnimated=False)
-    target_channels = get_channels(target, type="name", forbidden_channels=forbidden_channels, isAnimated=False)
+    tagsSource = dict()
+    for child in sourceGroup.children(recursive=True):
+        tag = get_tags(child)
+        tagsSource[tag] = child
 
-    differences = list(set(source_channels) - set(target_channels))
+    tagsTarget = dict()
+    for child in targetGroup.children(recursive=True):
+        tag = get_tags(child)
+        tagsTarget[tag] = child
 
-    for difference in differences:
-        forbidden_channels.append(difference)
+    tagMismatch = []
+    for tag in tagsSource:
+        print tag
+        if tag in tagsTarget:
+            print("{1} → {0}".format(tagsTarget[tag].name,tagsSource[tag].name) )
+            reload(bd_transfer_anim)
+            bd_transfer_anim.main(source=tagsSource[tag], target=tagsTarget[tag])
+        else:
+            tagMismatch.append(tagsSource[tag].name)
 
-    for channel in source.channels():
-        if channel.isAnimated:
-            if channel.name not in forbidden_channels:
-                if channel.envelope.keyframes.numKeys > 0:
-                    pprint(channel.name)
-                    channel_copy_paste(source.id, channel.name, cmd="copy")
-                    channel_copy_paste(target.id, channel.name, cmd="paste")
-
-    for source_transform in source.transforms:
-        source_transform_type = source_transform.type
-        exists = False
-
-        for target_transform in target.transforms:
-            if target_transform.type == source_transform_type:
-                exists = True
-                target_transform_id = target_transform.id
-
-        if not exists:
-            target_transform_id = target.transforms.insert(source_transform_type)
-            target_transform_id = target_transform_id.id
-
-        for channel in source_transform.channels():
-            if channel.isAnimated:
-                if channel.name not in forbidden_channels:
-                    if channel.envelope.keyframes.numKeys > 0:
-                        channel_copy_paste(source_transform.id, channel.name, cmd="copy")
-                        channel_copy_paste(target_transform_id, channel.name, cmd="paste")
-
-    if len(differences) != 0:
-        warning = "The following channels could not be copied because they do not exist on the target item:\n{0}".format(
-        differences)
-        modo.dialogs.alert("Warning", warning, dtype='warning')
+    if len(tagMismatch) > 0:
+        message = ""
+        for tag in tagMismatch:
+            message = "{}\n{}".format(message, tag)
+        modo.dialogs.alert("Warning",
+                           "There were animated items that could not be matched to any items in the target.\n"
+                           "Check their anim tag. Maybe it is missing or there is a mismatch.\n"
+                           "{}".format(message), dtype='warning')
 
 
 # END MAIN PROGRAM -----------------------------------------------
